8507b475ec875c672f53cbef5adb48700eaafe79
shiphan-pathan <shiphanpathan2001@gmail.com>
2025-08-05 20:09:11 +0530

    api

--- Frontend/src/service/product.service.js
+++ Frontend/src/service/product.service.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3030";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3030";
+
+
 
 class ProductService{
         saveProduct(product){
